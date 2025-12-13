c533fd95551648a4a807be17b973c35bdab80b17
Pepsi-Lin <lyqcs501@gmail.com>
2025-12-13 21:07:51 +0800

    Update qiime2_ubuntu_script.sh

--- qiime2_ubuntu_script.sh
+++ qiime2_ubuntu_script.sh
@@ -1,25 +1,17 @@
 # 设置工作目录为变量，方便多次使用
-wd=~/github/QIIME2ChineseManual/tutorials
+wd=~/QIIME2ChineseManual/tutorials
 
 # 进入tutorials工作目录
 cd $wd
 
 # ===== 激活QIIME 2工作环境 =====
 # 您的环境名称是 qiime2（不带版本号后缀）
-conda activate qiime2
+conda activate qiime2 2024.2.0
 # 命令行前面会显示 (qiime2) 表示成功进入工作环境
-
-# ===== 验证QIIME 2版本 =====
-qiime --version
-# 显示：q2cli version 2024.2.0
-
-# ===== 查看更详细的版本信息 =====
-qiime info
-# 这会显示所有QIIME 2插件和依赖的详细版本
 
 # ===== 验证当前工作路径 =====
 pwd
-# 确认您在 ~/github/QIIME2ChineseManual/tutorials 目录
+# 确认您在 ~/QIIME2ChineseManual/tutorials 目录
 
 # ===== 列出当前目录内容 =====
 ls -lh
